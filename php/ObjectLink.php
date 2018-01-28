df7c2dedae8a1be05dce5fa214d71de2df09c30e
Вячеслав Кульша <vkulsha@mail.ru>
2018-01-28 22:13:32 +0300

    paint marker arendator

--- php/ObjectLink.php
+++ php/ObjectLink.php
@@ -673,7 +673,7 @@ class ObjectLink {
 		try {
 			$oid = (isset($params[0]) && $params[0]) ? $params[0] : 0;
 			$func = (isset($params[1]) && $params[1]) ? $params[1] : "Marker";
-			$param = (isset($params[2]) && $params[2]) ? $params[2] : '{"weight":1, "color":"#ffff00"}';
+			$param = (isset($params[2]) && $params[2]) ? $params[2] : ($func == "Marker" ? '' : '{"weight":1, "color":"#ffff00"}');
 			$coords = (isset($params[3]) && $params[3]) ? $params[3] : "";
 			if (!$oid) return 0;
 			

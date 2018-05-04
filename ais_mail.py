46a5bdd5538904d2db5c96d7d8b2ee484bb82952
Кульша Вячеслав Игоревич <KulshaVI@guss.ru>
2018-05-04 11:59:05 +0300

    python mail calendar

---
+++ ais_mail.py
@@ -0,0 +1,21 @@
+﻿import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def sendmail(address_book, body):
+	address_book = ['kulshavi@guov.ru', 'kulshavi@guov.ru']
+	msg = MIMEMultipart()    
+	sender = 'AnaliticInformationSystem@guov.ru (don`t reply)'
+	subject = "Аналитическая Информационная Система - Календарь - Напоминание..."
+	#body = "Сдать отчетность НВОС 123..."
+
+	msg['From'] = sender
+	msg['To'] = ','.join(address_book)
+	msg['Subject'] = subject
+	msg.attach(MIMEText(body, 'plain'))
+	text=msg.as_string()
+
+	s = smtplib.SMTP('mail.guov.ru')
+	s.sendmail(sender,address_book, text)
+	s.quit() 
+	return 1    
---
+++ httprequest.py
@@ -0,0 +1,20 @@
+﻿from datetime import date
+from urllib.request import urlopen
+import urllib.parse
+import json
+import ais_mail
+
+base_url = 'http://reutov-srv01.guss.ru/php/olp.php?'
+today = str(date.today().strftime('%d.%m.%Y'))
+params = urllib.parse.urlencode({'f':'gT2' ,'p':'[["Календарь","Дата","Дата напоминания","Описание","Email"],[],[],false,null,"and (`Дата напоминания` =\''+today+'\' or `Дата` =\''+today+'\') "]', 'u':1577})
+
+url = base_url + params
+
+data = urlopen(url).read().decode("utf-8")
+jsdata = json.loads(data)
+
+for row in jsdata:
+	email = row[9]
+	body = row[3] + " - " + row[7]
+	ais_mail.sendmail([email], body)
+
